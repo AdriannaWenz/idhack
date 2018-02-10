5a84c24b7e5065043d40cec1e364c90fabe06497
Adrianna Wenz <adrianna_wenz@brown.edu>
2018-02-10 11:59:57 -0500

    finished for Pres

--- app/models.py
+++ app/models.py
@@ -1,3 +1,10 @@
+# <One Light Global>
+# Copyright © <2018> <MIT>
+# Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+# The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+
 from datetime import datetime
 from app import db
 
@@ -11,11 +18,18 @@ class User(db.Model):
     def __repr__(self):
         return '<User {}>'.format(self.username)
 
+class Conversation(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    user1_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+    user2_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+    def __repr__(self):
+        return '<Conversation {}>'.format(self.user1_id)
+
 class Post(db.Model):
     id = db.Column(db.Integer, primary_key=True)
+    convId = db.Column(db.Integer, db.ForeignKey('conversation.id'))
     body = db.Column(db.String(200))
     timestamp = db.Column(db.DateTime, index=True, default=datetime.utcnow)
     user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
-
     def __repr__(self):
         return '<Post {}>'.format(self.body)
--- app/routes.py
+++ app/routes.py
@@ -1,7 +1,15 @@
-from flask import render_template
+# <One Light Global>
+# Copyright © <2018> <MIT>
+# Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+# The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+
+from flask import render_template, request
 from app import app
 from app import db
-from app.models import User, Post
+from app.models import User, Post, Conversation
+from sqlalchemy import and_, or_
 
 @app.route('/')
 @app.route('/index')
@@ -11,4 +19,11 @@ def index():
 def experts():
     expertz = User.query.all()
     return render_template('experts.html', title="Experts", experts = expertz)
-@app.route('/chat')
+@app.route('/conversation', methods=['GET', 'POST'])
+def conversation():
+    expert = request.args.get('expert', None)
+    anon = User.query.filter_by(username="Anonymous")
+    #convo = Conversation.query.filter_by(or_(user1_id=expert.id, user2_id=anon.id), (user2_id=expert.id, user1_id=anon.id)).first_or_404()
+    convo = Conversation.query.get(1)
+    posts = Post.query.filter_by(convId=convo.id)
+    return render_template('conversation.html', title="Conversation", posts = posts)
